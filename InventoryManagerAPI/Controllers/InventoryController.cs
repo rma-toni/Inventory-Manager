158a98f6fda1927709aab372daa9bf8833cd6a31
rma-toni <rmaflores.sw@gmail.com>
2026-01-05 22:46:29 -0300

    initial commit

---
+++ InventoryManagerAPI/Controllers/InventoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventoryManagerAPI.Controllers.Models;
+
+namespace InventoryManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // La ruta será: api/inventario
+
+    public class InventoryController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", StockQuantity = 10 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public ActionResult PostProduct(Product newProduct)
+        {
+            products.Add(newProduct);
+            return CreatedAtAction(nameof(GetProducts), new { id = newProduct.Id }, newProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return NoContent();
+        }
+    }
+}
---
+++ InventoryManagerAPI/Controllers/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagerAPI.Controllers.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
